eed7edaeb308f432df73281936f1e331f6f024ff
Stephan Skanta <78694630+techcree@users.noreply.github.com>
2022-12-10 09:55:34 +0100

    Update secret.py

--- secret.py
+++ secret.py
@@ -4,5 +4,5 @@
 #ssid = 'SKANTAPico' #Your network name
 #password = '12345678' #Your WiFi password
 
-ssid = 'Voyager' #Your network name
-password = '1611688611614994' #Your WiFi password
+ssid = 'DEINWLAN' #Your network name
+password = 'WLANPASSWORT' #Your WiFi password
